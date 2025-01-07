2cd5a5dbf194a6c3ac81a94ab1bd52795824be21
Fernando Cordova <fernandocordova1233@gmail.com>
2025-01-06 19:33:13 -0500

    fixed middleware

--- server/middlewares/session.js
+++ server/middlewares/session.js
@@ -5,24 +5,17 @@ const JWT_KEY = process.env.JWT_KEY
 
 const sessionValidation = async (req, res, next) => {
     try {
-        // Preflight request -- verifies if endpoint accepts HTTP methods
         if (req.method === "OPTIONS") next()
-        // Check if token has been provided
         if (!req.headers.authorization) throw new Error("Forbidden")
-        // Sanitize the token to remove word 'Bearer' if it exists
         const authToken = req.headers.authorization.includes("Bearer")
             ? req.headers.authorization.split(" ")[1]
             : req.headers.authorization
-        // Extricate payload and verify token authenticity
         const payload = jwt.verify(authToken, JWT_KEY)
 
-        // Call users collectio nfind user matching payload id
         const foundUser = await User.findById(payload._id)
 
-        // Add the user to the request
         req.body.user = { userID: foundUser._id, fullName: foundUser.fullName }
 
-        // Continue to the next function
         next()
     } catch(err) {
         console.error(err)
