d07f9140596f0110ac276cc48becf4facca32b5f
Fernando Cordova <fernandocordova1233@gmail.com>
2025-01-09 18:06:00 -0500

    completed auth

--- client/src/Components/LoginSignUp/LoginSignUp.jsx
+++ client/src/Components/LoginSignUp/LoginSignUp.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react'
+import { useState} from 'react'
 import './loginSignUp.css'
 
 import logo_icon from '../Assets/logo.png'
@@ -11,13 +11,35 @@ export default function LoginSignUp({ updateLocalStorage }) {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [login, setLogin] = useState(true)
+    const [message, SetMessage] = useState("")
+
+    const toggle = () => {
+        setLogin(!login);
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setPassword("");
+        SetMessage("");
+    }
+
+    const toggleBtn = () => (login ? "Register" : "Login");
+
+    const register = () => login ? null : (
+        <>
+            <div className="input">
+                <input onChange={e => setFirstName(e.target.value)} value={firstName} type="text" name="fName" placeholder="First Name" id="Fname" />                </div>
+            <div className="input">
+                <input onChange={e => setLastName(e.target.value)} value={lastName} type="text" name="lName" placeholder="Last Name" id="Lname" />
+            </div>
+        </>
+    )
 
     const handleSubmit = event => {
         event.preventDefault()
 
         const url = login
-            ? "http://127.0.0.1:4000/auth/login"
-            : "http://127.0.0.1:4000/auth/register"
+            ? "http://127.0.0.1:4000/user-routes/login"
+            : "http://127.0.0.1:4000/user-routes/register"
 
             const body = login
             ? { email, password }
@@ -30,27 +52,18 @@ export default function LoginSignUp({ updateLocalStorage }) {
                 "Content-Type": "application/json"
             })
         })
-            .then(res => res.json())
-            .then(data => updateLocalStorage(data.token))
-    } 
+        .then(res => res.json())
+        .then(data => updateLocalStorage(data.token))
+}
 
     return ( 
     <div className="login-container">
         <div className="header">
             <img className="header-img" src={logo_icon} alt="WebKIDSS Logo" />
-            <div className="text">{action}</div>
+            <div className="text">{login ? "Login" : "Register"}</div>
         </div>
         <form action="" onSubmit={handleSubmit} className="login-inputs">
-            {action==="Login"?<div></div>:
-                <>
-                    <div className="input">
-                        <input onChange={e => setFirstName(e.target.value)} value={firstName} type="text" placeholder="First Name" id="Fname" />
-                    </div>
-                    <div className="input">
-                        <input onChange={e => setLastName(e.target.value)} value={lastName} type="text" placeholder="Last Name" id="Lname" />
-                    </div>
-                </>
-            }
+            {register()}
             <div className="input">
                 <input onChange={e => setEmail(e.target.value)} value={email} type="email" placeholder="Email" id="email" />
             </div>
@@ -58,10 +71,11 @@ export default function LoginSignUp({ updateLocalStorage }) {
                 <input onChange={e => setPassword(e.target.value)} value={password} type="password" placeholder="Password" id="pwd" />
             </div>
             <div className="submit-container">
-                <input type="submit" className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Register")}} value="Register" />
-                <input type="submit" className={action==="Register"?"submit gray":"submit"} onClick={()=>{setAction("Login")}} value="Login" />
+                <input type="submit" className={action==="Login"?"gray":"submit"} id="toggle" onClick={toggle} value={toggleBtn()} />
+                <input type="submit" className={action==="Register"?"gray":"submit"} onClick={handleSubmit} value={login ? "Login" : "Sign Up"} />
             </div>
         </form>
+        {message && <p className="message">{message}</p>}
     </div>
     )
 }
