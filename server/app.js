b2499701468f252decba6d1294e63b3c8a16ba37
Fernando Cordova <fernandocordova1233@gmail.com>
2025-01-09 18:08:03 -0500

    fixed bugs with user-routes and app

--- server/app.js
+++ server/app.js
@@ -1,17 +1,21 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
-
+const cors = require('cors');
 const PORT = process.env.PORT
 const HOST = process.env.HOST
 
 const { dbConnect } = require("./db")
+const sessionValidation = require("./middlewares/session");
 const eventsController = require("./controllers/eventRoutes")
+const authController = require("./controllers/user-routes");
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
-app.use("/events", eventsController)
+app.use("/user-routes", authController);
+app.use("/events", sessionValidation, eventsController)
 
 app.listen(PORT, HOST, () => {
     dbConnect(),
