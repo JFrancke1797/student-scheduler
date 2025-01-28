052e10ea08c95a87f1f1fd3de943d7c422ac7b03
John Murphy <jmurphy.org@gmail.com>
2025-01-27 20:18:12 -0500

    remove state

--- client/src/Components/Calender.jsx
+++ client/src/Components/Calender.jsx
@@ -1,113 +1,106 @@
 "use client"
-import React ,{ useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridView from '@fullcalendar/daygrid'
 import timeGridView from '@fullcalendar/timegrid'
-import interactionPlugin, {Draggable} from '@fullcalendar/interaction'
+import interactionPlugin, { Draggable } from '@fullcalendar/interaction'
 import listPlugin from '@fullcalendar/list'
 import '../Components/Calender.css'
-import modalPlugin from '@fullcalendar/interaction'
-
-import DropArg from '@fullcalendar/interaction'
-
-
 
 export default function Calender() {
+  const [events, setEvents] = useState([])
+  const [allEvents, setAllEvents] = useState([])
+  const [isRemoveMode, setIsRemoveMode] = useState(false) // State to track "remove mode"
 
- 
-    const [events, setEvents] = useState([])
+  const calendarRef = useRef(null) // Reference for FullCalendar
 
-    const [allEvents, setAllevents] = useState([])
-    const [newEvent, setNewEvent] = useState({
-      title: '',
-      start: '',
-      end: '',
-      allDay: false,
-      id: 0
-    })
+  useEffect(() => {
+    let dropTing = document.getElementById('draggable-el')
 
-    useEffect(() => {
-      
-      let dropTing = document.getElementById('draggable-el')
-      
-      if(dropTing){
-        new Draggable(dropTing,{
-          itemSelector: 'div',
-          eventData: function(eventEl){
-            let title = eventEl.getAttribute('title')
-            let id = eventEl.getAttribute('data')
-            let start = eventEl.getAttribute('start')
+    if (dropTing) {
+      new Draggable(dropTing, {
+        itemSelector: 'div',
+        eventData: function (eventEl) {
+          let title = eventEl.getAttribute('title')
+          let id = eventEl.getAttribute('data')
+          let start = eventEl.getAttribute('start')
 
-            return{title, id, start}
-          }
-        })
+          return { title, id, start }
+        }
+      })
+    }
+  }, [])
+
+  function addEvent(data) {
+    const event = {
+      title: data.draggedEl.innerText,
+      start: data.date.toISOString(),
+      allDay: data.allDay,
+      id: new Date().getTime().toString() // Generate a unique ID
+    }
+    setAllEvents([...allEvents, event])
+  }
+
+  function handleAddEventInput() {
+    const addedEvent = document.getElementById('inputs').value
+    document.getElementById('inputs').value = ''
+    const newId = new Date().getTime().toString() // Generate a unique ID
+    setEvents([...events, { title: addedEvent, id: newId }])
+    setAllEvents([...allEvents, { title: addedEvent, start: '', allDay: false, id: newId }])
+  }
+
+  function toggleRemoveMode() {
+    setIsRemoveMode(!isRemoveMode) // Toggle remove mode on/off
+  }
+
+  function handleEventClick(info) {
+    if (isRemoveMode) {
+      if (window.confirm(`Are you sure you want to delete the event: "${info.event.title}"?`)) {
+        info.event.remove() // Remove the clicked event
+        console.log(`Event with ID ${info.event.id} removed.`)
       }
-
-    },[])
-
-    function clickTing (arg){
-      //!This was a test function that might still be used
-      setNewEvent({...newEvent, start: arg.date, allDay: arg.allDay, id: new Date().getDate()})
-      console.log(arg)
+      setIsRemoveMode(false) // Turn off remove mode after deletion
+    } else {
+      console.log(`Clicked on event: ${info.event.title}`)
     }
-
-    function addEvent(data = DropArg){
-      
-      const event = {...newEvent, start: data.date.toISOString(), title: data.draggedEl.innerText, allDay: data.allDay}
-      setAllevents([...allEvents, event])
-    }
-
-    function handelAddEventInput(){
-
-      const addedEvent = document.getElementById('inputs').value
-      document.getElementById('inputs').value = ''
-      setEvents([...events, {title: addedEvent, start: '', allDay: '', id: events.id}])
-    
-    }
-
-
+  }
 
   return (
     <>
-    
-    <div id='Apptings'>
-     <div id='draggable-el'>
-  
+      <div id="Apptings">
+        <div id="draggable-el">
           <h1>Drag Events</h1>
-          {events.map((tings,i) => (
-            <div 
-              title= {tings.title}
-              key={i}
-            >
+          {events.map((tings, i) => (
+            <div title={tings.title} key={i}>
               {tings.title}
             </div>
           ))}
         </div>
-          <h1>Add An Event</h1>
-          <input type="text" placeholder='add an event' id='inputs' />
-          <button onClick={handelAddEventInput}>Add</button>
-          <button>Remove</button>
+        <h1>Add An Event</h1>
+        <input type="text" placeholder="add an event" id="inputs" />
+        <button onClick={handleAddEventInput}>Add</button>
+        <button onClick={toggleRemoveMode} style={{ backgroundColor: isRemoveMode ? 'red' : '' }}>
+          {isRemoveMode ? 'Cancel Remove Mode' : 'Remove'}
+        </button>
 
-    <h2>Teacher Scheduel</h2>
-      <FullCalendar
-        plugins={[dayGridView,timeGridView,interactionPlugin,listPlugin, modalPlugin]}
-        headerToolbar={{
+        <h2>Teacher Schedule</h2>
+        <FullCalendar
+          ref={calendarRef} // Attach the ref to FullCalendar
+          plugins={[dayGridView, timeGridView, interactionPlugin, listPlugin]}
+          headerToolbar={{
             center: 'title',
             left: 'dayGridMonth,timeGridWeek,dayGridDay,listWeek'
-        }}
-        nowIndicator={true}
-        droppable = {true}
-        selectable = {true}
-        editable = {true}
-        listDaySideFormat
-        drop={(data) => addEvent(data)}
-        eventClick={(info) => {
-          console.log(info)
-        }}
-        dayMaxEventRows={true}
-      />
-    </div>
-       
+          }}
+          nowIndicator={true}
+          droppable={true}
+          selectable={true}
+          editable={true}
+          drop={(data) => addEvent(data)}
+          events={allEvents} // Use the `allEvents` state for calendar events
+          eventClick={handleEventClick} // Handle click to remove event if in remove mode
+          dayMaxEventRows={true}
+        />
+      </div>
     </>
   )
 }
